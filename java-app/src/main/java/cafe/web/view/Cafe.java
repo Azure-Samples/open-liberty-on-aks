326cd6afeec30e825610466558e966e3e1ca4819
Jianguo Ma <jiangma@microsoft.com>
2025-04-23 12:15:45 +0800

    refine java-app per reza's comments
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- java-app/src/main/java/cafe/web/view/Cafe.java
+++ java-app/src/main/java/cafe/web/view/Cafe.java
@@ -22,71 +22,64 @@ import java.util.List;
 @RequestScoped
 public class Cafe implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private String baseUri;
+    private Client client;
 
-	private String baseUri;
-	private transient Client client;
+    @NotNull
+    @NotEmpty
+    private String name;
+    @NotNull
+    private Double price;
+    private List<Coffee> coffeeList;
 
-	@NotNull
-	@NotEmpty
-	protected String name;
-	@NotNull
-	protected Double price;
-	protected List<Coffee> coffeeList;
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Double getPrice() {
+        return price;
+    }
 
-	public Double getPrice() {
-		return price;
-	}
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public List<Coffee> getCoffeeList() {
-		this.getAllCoffees();
-		return coffeeList;
-	}
+    public List<Coffee> getCoffeeList() {
+        this.getAllCoffees();
+        return coffeeList;
+    }
 
   public String getHostName() {
     return System.getenv("HOSTNAME");
   }
 
-	@PostConstruct
-	private void init() {
-		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-				.getRequest();
-		baseUri = "http://localhost:9080" + request.getContextPath() + "/rest/coffees";
-		this.client = ClientBuilder.newBuilder().build();
+    @PostConstruct
+    private void init() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+                .getRequest();
+        baseUri = "http://localhost:9080" + request.getContextPath() + "/rest/coffees";
+        this.client = ClientBuilder.newBuilder().build();
+    }
 
-		name = (String) request.getSession().getAttribute("coffeeName");
-		price = (Double) request.getSession().getAttribute("coffeePrice");
-	}
+    private void getAllCoffees() {
+        this.coffeeList = this.client.target(this.baseUri).path("/").request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Coffee>>() {
+                });
+    }
 
-	private void getAllCoffees() {
-		this.coffeeList = this.client.target(this.baseUri).path("/").request(MediaType.APPLICATION_JSON)
-				.get(new GenericType<List<Coffee>>() {
-				});
-	}
+    public void addCoffee() throws IOException {
+        Coffee coffee = new Coffee(this.name, this.price);
+        this.client.target(baseUri).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("");
+    }
 
-	public void addCoffee() throws IOException {
-		Coffee coffee = new Coffee(this.name, this.price);
-		this.client.target(baseUri).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
-		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
-		request.getSession().setAttribute("coffeeName", this.name);
-		request.getSession().setAttribute("coffeePrice", this.price);
-		FacesContext.getCurrentInstance().getExternalContext().redirect("");
-	}
-
-	public void removeCoffee(String coffeeId) throws IOException {
-		this.client.target(baseUri).path(coffeeId).request().delete();
-		FacesContext.getCurrentInstance().getExternalContext().redirect("");
-	}
+    public void removeCoffee(String coffeeId) throws IOException {
+        this.client.target(baseUri).path(coffeeId).request().delete();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("");
+    }
 }
